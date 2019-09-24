3bc6f1bb37f3662b08959eb492998633a18adae7
dheoprasetyo <dheoprasetyo.dp@gmail.com>
2019-09-24 16:36:19 +0700

    manage categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,9 +11,17 @@ class CategoryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        
+        $filterKeyword = $request->get('name');
+
+        $categories = \App\Category::paginate(10);
+
+        if($filterKeyword){
+            $categories = \App\Category::where("name", "LIKE", "%$filterKeyword%")->paginate(10);
+            }
+
+        return view('categories.index', ['categories' => $categories]);
     }
 
     /**
@@ -34,7 +42,19 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $name = $request->get('name');
+        $new_category = new \App\Category;
+        $new_category->name = $name;
+
+        if($request->file('image')){
+            $image_path = $request->file('image')->store('category_images', 'public');
+            $new_category->image = $image_path;
+        }
+        $new_category->created_by = \Auth::user()->id;
+        $new_category->slug = str_slug($name, '-');
+        $new_category->save();
+
+        return redirect()->route('categories.create')->with('status', 'Category successfully created');
     }
 
     /**
@@ -45,7 +65,8 @@ class CategoryController extends Controller
      */
     public function show($id)
     {
-        //
+        $category = \App\Category::findOrFail($id);
+        return view('categories.show', ['category' => $category]);
     }
 
     /**
@@ -56,7 +77,8 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $category_to_edit = \App\Category::findOrFail($id);
+        return view('categories.edit', ['category' => $category_to_edit]);  
     }
 
     /**
@@ -68,7 +90,24 @@ class CategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $name = $request->get('name');
+        $slug = $request->get('slug');
+        $category = \App\Category::findOrFail($id);
+        $category->name = $name;
+        $category->slug = $slug;
+
+        if($request->file('image')){
+            if($category->image && file_exists(storage_path('app/public/' . $category->image))){
+                \Storage::delete('public/' . $category->name);
+            }
+                $new_image = $request->file('image')->store('category_images','public');
+                $category->image = $new_image;
+        }
+        $category->updated_by = \Auth::user()->id;
+        // mengupdate slug berdasarkan nama baru category yang disubmit dari form edit,
+        $category->slug = str_slug($name);
+        $category->save();
+        return redirect()->route('categories.edit', ['id' => $id])->with('status', 'Category successfully edited');
     }
 
     /**
@@ -79,6 +118,39 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $category = \App\Category::findOrFail($id);
+        $category->delete();
+
+        return redirect()->route('categories.index')->with('status', 'Category successfully moved to trash');
+    }
+
+    // menampilkan daftar Category yang sedang soft delete
+    public function trash(){
+        $deleted_category = \App\Category::onlyTrashed()->paginate(10);
+        return view('categories.trash', ['categories' => $deleted_category]);
+    //         Kode tersebut melakukan query ke database categories menggunakan model Category dan kita
+    // gunakan method onlyTrashed() untuk mendapatkan hanya kategori yang status nya soft delete yaitu yang
+    // field deleted_at nya tidak NULL.
+    // Setelah itu kita lempar sebagai variable $categories ke view categories.index.
+    }
+
+    public function restore($id){
+        $category = \App\Category::withTrashed()->findOrFail($id);
+        if($category->trashed()){
+            $category->restore();
+        } else {
+            return redirect()->route('categories.index')->with('status', 'Category is not in trash');
+        }
+            return redirect()->route('categories.index')->with('status', 'Category successfully restored');
+    }
+
+    public function deletePermanent($id){
+        $category = \App\Category::withTrashed()->findOrFail($id);
+        if(!$category->trashed()){
+            return redirect()->route('categories.index')->with('status', 'Can not delete permanent active category');
+        } else {
+            $category->forceDelete();
+            return redirect()->route('categories.index')->with('status', 'Category permanently deleted');
+        }
     }
 }
